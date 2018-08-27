2d74c65cdf5b150d3445095750ac0c5147bab1fc
emzan <zanettiemilio@hotmail.com>
2018-08-27 14:51:04 +0800

    luno

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -136,7 +136,7 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('QACtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page20Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
